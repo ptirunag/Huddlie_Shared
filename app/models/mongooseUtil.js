9ac7159f6bef6e382f9c6dac81b5c681962b03ea
Puneeth Tirunagari <puneeth.tirunagari@outlook.com>
2019-02-01 02:59:03 +0530

    test

--- app/models/mongooseUtil.js
+++ app/models/mongooseUtil.js
@@ -2,41 +2,41 @@
 var mongoose = require ('mongoose');
 
 // var dbURI = process.env.MONGODB_URI || 'localhost:27017/Huddlie';
-var dbURI2 = 'mongodb://huddlieServices9!:servicesMaster9!@ds151994.mlab.com:51994/heroku_gvn2dr71';
+// var dbURI2 = 'mongodb://huddlieServices9!:servicesMaster9!@ds151994.mlab.com:51994/heroku_gvn2dr71';
 var dbURI = 'mongodb://huddlieServices9!:servicesMaster9!@ds217475-a0.mlab.com:17475,ds217475-a1.mlab.com:17475/heroku_kw3mx5qs?replicaSet=rs-ds217475';
 
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://huddlieServices9!:servicesMaster9!@ds217475-a0.mlab.com:17475,ds217475-a1.mlab.com:17475/heroku_kw3mx5qs?replicaSet=rs-ds217475", function(err, db) {
-  // test.equal(null, err);
-  // test.ok(db != null);
-  console.log('CONNECTED USING MONGOCLIENT');
-  if (err)
-  {
-  	console.log('MONGOCLIENT ERR:'+err);
-  }
-  else
-  {
-  	console.log('MONGOCLIENT DBNAME = ');
-  	db.db('heroku_kw3mx5qs').collection('amazonSearchCacheStagingColl').findOne({}, function(err2, res2){
-  		if (err2)
-  		{
-  			console.log('MONGOCLIENT FINDONE Err'+err2);
-  		}
-  		else
-  		{
-  			console.log(res2);
-  		}
-  	})
-  }
-  // db.collection("replicaset_mongo_client_collection").update({a:1}, {b:1}, {upsert:true}, function(err, result) {
-  //   test.equal(null, err);
-  //   test.equal(1, result);
+// MongoClient.connect("mongodb://huddlieServices9!:servicesMaster9!@ds217475-a0.mlab.com:17475,ds217475-a1.mlab.com:17475/heroku_kw3mx5qs?replicaSet=rs-ds217475", function(err, db) {
+//   // test.equal(null, err);
+//   // test.ok(db != null);
+//   console.log('CONNECTED USING MONGOCLIENT');
+//   if (err)
+//   {
+//   	console.log('MONGOCLIENT ERR:'+err);
+//   }
+//   else
+//   {
+//   	console.log('MONGOCLIENT DBNAME = ');
+//   	db.db('heroku_kw3mx5qs').collection('amazonSearchCacheStagingColl').findOne({}, function(err2, res2){
+//   		if (err2)
+//   		{
+//   			console.log('MONGOCLIENT FINDONE Err'+err2);
+//   		}
+//   		else
+//   		{
+//   			console.log(res2);
+//   		}
+//   	})
+//   }
+//   // db.collection("replicaset_mongo_client_collection").update({a:1}, {b:1}, {upsert:true}, function(err, result) {
+//   //   test.equal(null, err);
+//   //   test.equal(1, result);
 
-  //   db.close();
-  //   test.done();
-  // });
-});
+//   //   db.close();
+//   //   test.done();
+//   // });
+// });
 
 var options = {
 	useMongoClient : true
